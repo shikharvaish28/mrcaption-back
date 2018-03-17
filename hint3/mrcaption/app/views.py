52b9664c951e27f4ab8ee4f28cc25973eb50cc74
shikharvaish28 <shikarvaish@gmail.com>
2018-03-17 15:18:37 +0530

    returning json file after upload

--- hint3/mrcaption/app/views.py
+++ hint3/mrcaption/app/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+from django.http import JsonResponse
 import requests
 # @csrf_exempt
 def get_image(request):
     if request.method == 'POST':
-        img = request.FILES["image"]
+        img = request.FILES.get("image")
         path = default_storage.save('mrcaption/image', ContentFile(img.read()))
-        print (path)
+        print (request.FILES)
+        # print (request.body)
         headers  = {'Ocp-Apim-Subscription-Key': '2593b2ee7a9345c7823c2dd3df0f028d',
         "Content-Type": "application/octet-stream" }
         image_data = open(path , "rb").read()
@@ -16,7 +18,6 @@ def get_image(request):
         # data     = {'url': "mrcaption/image"}
         vision_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories,Tags&language=en"
         response = requests.post(vision_url, headers=headers, data=image_data)
-        print (response.json())
-        html = "<html><body><h1>hey</h1></body></html>"
-        
-        return HttpResponse(html)
+        data = response.json()
+        # print (data)        
+        return JsonResponse(data)
