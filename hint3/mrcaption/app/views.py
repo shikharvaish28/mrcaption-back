995dc600226c64d9df0c93a49bea28133845f8ec
shikharvaish28 <shikarvaish@gmail.com>
2018-03-17 15:19:32 +0530

    minor fixes

--- hint3/mrcaption/app/views.py
+++ hint3/mrcaption/app/views.py
@@ -10,12 +10,9 @@ def get_image(request):
         img = request.FILES.get("image")
         path = default_storage.save('mrcaption/image', ContentFile(img.read()))
         print (request.FILES)
-        # print (request.body)
         headers  = {'Ocp-Apim-Subscription-Key': '2593b2ee7a9345c7823c2dd3df0f028d',
         "Content-Type": "application/octet-stream" }
         image_data = open(path , "rb").read()
-        # params   = {'visualFeatures': 'Categories,Tags'}
-        # data     = {'url': "mrcaption/image"}
         vision_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories,Tags&language=en"
         response = requests.post(vision_url, headers=headers, data=image_data)
         data = response.json()
