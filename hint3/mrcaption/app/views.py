925b4977ccc25f169ae2a7cc61da1ff5b1b135b6
shikharvaish28 <shikarvaish@gmail.com>
2018-03-17 13:28:36 +0530

    sending data to asure but return is not expected

--- hint3/mrcaption/app/views.py
+++ hint3/mrcaption/app/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 import requests
 # @csrf_exempt
 def get_image(request):
     if request.method == 'POST':
-        # img = request.POST.get(" image ")
-        # return (request)
-        # print (request.body)
         img = request.FILES["image"]
+        path = default_storage.save('mrcaption/image', ContentFile(img.read()))
+        # print (type(img))
+        headers  = {'Ocp-Apim-Subscription-Key': '2593b2ee7a9345c7823c2dd3df0f028d',
+        "Content-Type": "application/json" }
+        image_data = open('mrcaption/image', "rb").read()
+        # params   = {'visualFeatures': 'Categories,Tags'}
+        # data     = {'url': "mrcaption/image"}
+        vision_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories,Tags&language=en"
+        response = requests.post(vision_url, headers=headers, data=image_data)
+        print (response.json())
         html = "<html><body><h1>hey</h1></body></html>"
         
         return HttpResponse(html)
